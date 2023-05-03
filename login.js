e3080b195c0ff51d8d5d4ab5d017e8935e18a6de
praveen <pngupta@gainsight.com>
2023-05-03 07:10:01 +0530

    adding apptrinstic value

--- login.js
+++ login.js
@@ -1,3 +1,15 @@
+// <!-- Gainsight PX Tag-->
+
+(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+    (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+  var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+  var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-HSFMXCXG5UBB-2");  
+
+// <!-- Gainsight PX Tag-->
+
+
+
 function validateData() {
     // debugger
     const li = ["pnxt1", "pnxt2", "pnxt3", "pnxt4"];
@@ -6,7 +18,23 @@ function validateData() {
     var password = document.getElementById("pswrd").value;
     
     if (li.includes(username) && password==="test@1234" ){
-        console.log("user : ",username);
+        // console.log("user : ",username);
+        //passing user and account objects:
+        aptrinsic("identify",
+        {
+            //User Fields
+              "id": username, // Required for logged in app users
+              "email": username.concat("@gs.com"),
+              "firstName": "Iron",
+              "lastName": "Man"
+            },
+            {
+            //Account Fields
+              "id": username, //Required
+              "name":"Iron Man Machine"
+            });
+
+
         alert('Login Success');
         return true;
         
